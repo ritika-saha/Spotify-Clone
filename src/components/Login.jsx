edd466323123bd0ccd2941a5365b940a006bf61d
ritika-saha <ritika882001@gmail.com>
2023-01-02 20:27:14 +0530

    login ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,17 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container=styled.div`
+ display: grid;
+ align-items: centre;
+ justify-content: center;
+ place-items: center;
+ background-color: black;
+ width: 100%;
+ height: 100vh;
+`
+
+const Image=styled.img`
+  width: 100%;
+  padding:0 15%;
 
 `
 
-const Image=styled.img``
+const Butn=styled.a`
+  color:white;
+  padding: 20px;
+  width: 200px;
+  height: 60px;
+  background-color: purple;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 99px;
+  text-decoration: none;
+  font-weight:600;
 
-const Button=styled.button``
+`
 function Login() {
   return (
     <Container>
         <Image src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" />
-        <Button>Login with Spotify</Button>
+        <Butn>Login with Spotify</Butn>
     </Container>
   )
 }
