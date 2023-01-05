0c879e8d927d313eec584800301ebe1b39793a64
ritika-saha <ritika882001@gmail.com>
2023-01-05 11:08:44 +0530

    fetching access token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './components/Login';
+import { getTokenFromResponse } from './spotify';
 
 function App() {
+  const [token,setToken]=useState(null);
+  useEffect(()=>{
+    const hash=getTokenFromResponse();
+    //clearing access token for security reasons
+    //console.log(hash)
+    window.location.hash="";
+    const curr_token=hash.access_token;
+    if(curr_token){
+    setToken(curr_token);
+    }
+  },[]);
+
   return (
     <div className="app">
     
